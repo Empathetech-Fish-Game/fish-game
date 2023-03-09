60bebfc5dc22d352849be3a7617b0fa2a31aa5da
Ben Beekman <bbeekman@gmail.com>
2023-03-08 18:53:03 -0800

    fish rotation randomized variation

--- src/components/LowPolyFish.jsx
+++ src/components/LowPolyFish.jsx
@@ -12,30 +12,58 @@ export default function LowPolyFish() {
 
   const rotation = [0, Math.PI / -2, 0];
 
-  const updateFishPathing = (targetFish) => {
-    const fishMovementInPlace = fishRef.current.rotation.y = (0.75 * Math.sin(time) * 0.5) / Math.PI;
 
-    if (targetFish.current.position.x < -4) {
-      targetFish.rotation = randomnumber
-    }
-    let rando = Math.random();
-    if (rando > 0.5) {
-
-    }
-
-
-
-  };
 
 
   useFrame(({ clock }) => {
     const time = clock.elapsedTime;
+
+    const updateFishPathing = (targetFish) => {
+      //const fishMovementInPlace = fishRef.current.rotation.y = (0.75 * Math.sin(time) * 0.5) / Math.PI;
+
+      if (targetFish.current.position.x < -4) {
+        targetFish.rotation = targetFish.rotation + 0; //I'm not sure what this is supposed to be
+      }
+      let rando = Math.floor(Math.random() * 100) + 1; //random int between 1 and 100
+
+      if (rando > 99) {
+        //don't alter your rotation this time
+      }
+      else {
+        let randRotationX = (Math.random() / 100);
+        let randRotationZ = (Math.random() / 100);
+        if (Math.random() < 0.5) {
+          randRotationX = -randRotationX;
+        }
+        if (Math.random() < 0.5) {
+          randRotationZ = -randRotationZ;
+        }
+
+        //rotate the fish along the X and Z axes:
+        fishRef.current.rotation.z - fishRef.current.rotation.z + randRotationZ;
+        fishRef.current.rotation.y = fishRef.current.rotation.y + randRotationX;
+        //set new rotation's X value to the old value + randRotationX
+        //set new rotation's Z value to the old value + randRotationZ
+      }
+
+      /*
+      TODO: maybe adjust the fish velocity to occasionally increase or decrease?
+      modify current velocity by adding or subtracting a small amount
+      */
+
+      /*
+      TODO: adjust the direction of movement relative to the rotation:
+      The fish should always travel straight forward from its POV
+      */
+    };
+
+    updateFishPathing(fishRef);
     //fishRef.current.position.x = Math.sin(time) * (Math.PI * -0.25);
     //fishRef.current.position.z = Math.cos(time) * (Math.PI * -0.5);
-    fishRef.current.rotation.y = (0.75 * Math.sin(time) * 0.5) / Math.PI;
+    //fishRef.current.rotation.y = (0.75 * Math.sin(time) * 0.5) / Math.PI;
   });
 
-  
+
 
   return (
     <group ref={fishRef} scale={0.1} position={[0, 0, 0]} rotation={rotation}>
